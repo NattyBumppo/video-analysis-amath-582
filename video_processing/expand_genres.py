13d6b022bf8dd23ca748b6645761adf8ca0e89d5
John Fuini <fuini@uw.edu>
2016-02-26 08:37:00 -0800

    Fixed it so it outputs a trailer_data_expanded_genre.csv
    wasn't doing that before

--- video_processing/expand_genres.py
+++ video_processing/expand_genres.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 """
+Created on Thu Feb 25 14:30:50 2016
 
-
-John Fuini, Grade 4, Mrs. Cotton, Orange Iguanas
-
-
+@author: fuini
 """
+
 
 import csv
 import re
 
 csv_filename = "trailer_data.csv"
 
+my_dict = {}
+
+# list of my genres
 no_genre_movie_list = []
 genre_list = []
+genre_count = {}
 
 #findin the genres and the movies without genres
 with open(csv_filename, 'r') as csv_file:
@@ -24,47 +27,55 @@ with open(csv_filename, 'r') as csv_file:
             genres = re.findall('(\w+)', row["genre"])
             for genre in genres:
                 if genre in genre_list:
-                    pass
+                    genre_count[genre] += 1
                 else:
                     genre_list.append(genre)
+                    genre_count[genre] = 1
+                    
             # holy shit that worked.
                     
         else:  #note who doesn't have a genre
             no_genre_movie_list.append(row["title"])
             
-print len(no_genre_movie_list)
+print "Number of movies with no genre: " + str(len(no_genre_movie_list))
 # 68 movies have no genre. 
+print "Number of unique genres: " + str(len(genre_list))
 print genre_list
+# 25 genres
+print "Each genre and number of times present in data: "
+print genre_count
+print "We should seriously consider removing genres that have only a few representatives, as we don't have the statistics to say anything meaningful."
 
-    
-# want to make an new csv that is like the old csv,
-# but has new columns for each genre, with a 0 or 1 
-# for it the movie is not or is that genre respectively
-    
-    
-    
-genre_dict = {}
 
+
+#Read in and save CSV into my_dict, while building new rows into my_dict
 with open(csv_filename, 'r') as csv_file:
     reader = csv.DictReader(csv_file)
-    temp_list = list(reader)
-    for genre in genre_list:
-        genre_dict[genre] = []
-        for i, row in enumerate(temp_list):
-            if i > 0:
-                genres = re.findall('(\w+)', row["genre"])      
-                if genre in genres:
-                    (genre_dict[genre]).append(1)
+    for i, row in enumerate(reader):
+        my_dict[row["title"]] = {}
+        for key in row.keys():            
+            my_dict[row["title"]][key] = row[key]
+            #collect genres of movie
+            genres_of_movie = re.findall('(\w+)', row["genre"])
+            #loop through all genres, append 0 or 1 for each genre
+            for genre in genre_list: 
+                if genre in genres_of_movie:
+                    my_dict[row["title"]][genre] = 1
                 else:
-                    (genre_dict[genre]).append(0)
-                    
-#print genre_dict        
-  
+                    my_dict[row["title"]][genre] = 0
+        # check genre business
+        # loop over my list of genres
+print my_dict["Step-Up-All-In"]
 
-"""
-OK Nat.  I have made a dictionary, with each key (genre) giving a list of numbers corresponding to the list of movies 
-in your csv.  They are ordered the same. 960 of them.   Now I need these dictionarys added as columns to the main csv. 
 
-"""
+#now write out my dict to a csv
+final_csv_filename = "trailer_data_expanded_genre.csv"
+with open(final_csv_filename, 'wb') as csv_file:
+    fieldnames = ['title', 'num_frames', 'total_time', 'avg_intensity', 'avg_color_r', 'avg_color_g', 'avg_color_b', 'avg_shot_length', 'num_shots', 'original_format_title', 'movie_list_webpage_url', 'genre', 'release_date', 'imdb_url', 'mean_volume', 'std_dev_volume', 'min_volume', 'max_volume', 'Drama', 'Horror', 'Thriller', 'Action', 'Comedy', 'Crime', 'Mystery', 'Sport', 'Romance', 'Biography', 'History', 'Animation', 'Adventure', 'Family', 'Documentary', 'Fantasy', 'Music', 'Western', 'Musical', 'Sports', 'Supernatural', 'War', 'News', 'Animaton', 'Short']
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-   
+    writer.writeheader()
+
+    for key in my_dict.keys():
+        writer.writerow(my_dict[key])
+
