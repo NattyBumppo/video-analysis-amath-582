507071bcd45c3f77482cbb1979f1503c3f2c5e8d
John Fuini <fuini@uw.edu>
2016-02-29 21:23:10 -0800

    Created the function that makes the tree.

---
+++ data_analysis/check_genre_predictions.m
@@ -0,0 +1,25 @@
+function A = check_genre_predictions(data, genre, fraction_to_train, num_trials)
+
+%tags for the genre in question
+failed_fractions = [];
+for j = 1:num_trials
+    [num_movies, num_features] = size([data]); 
+
+    rand_order = randperm(num_movies); % random ordering
+
+    train_to = floor(fraction_to_train*num_movies); 
+
+    train_data = data(rand_order(1:train_to),:);
+    test_data = data(rand_order(train_to+1:end),:);
+    train_labels =  genre(rand_order(1:train_to));
+    test_labels = genre(rand_order(train_to+1:end));
+
+    tree = fitctree(train_data, train_labels); % train on the first 80% of movies
+
+    prediction = predict(tree,test_data);
+    failed_fraction = sum(abs(test_labels-prediction))/(num_movies-train_to);
+    failed_fractions(j) = failed_fraction;
+    
+end
+
+A = sum(failed_fractions)/length(failed_fractions);
