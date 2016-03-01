15c04e3aa59ec0e36303ad81b90a10e970c89ba9
Noel Kimber <kimber.noel@outlook.com>
2016-03-01 03:15:20 -0800

    modified so that the saved mp3s have the same name convention as the trailer data csv. Also, had to run this on Windows, so ffmpeg path was changed

--- video_processing/extract_mp3s.py
+++ video_processing/extract_mp3s.py
@@ -1,14 +1,14 @@
 import os
 import sys
 
-video_dir = 'F:\\582 videos\\video_files'
-mp3_dir = 'F:\\582 videos\\mp3s'
+video_dir = 'C:\\Users\\Noel_K\\git_repository\\video-analysis-amath-582\\video_processing\\video_files'
+mp3_dir = 'C:\\Users\\Noel_K\\git_repository\\video-analysis-amath-582\\video_processing\\mp3s'
 
-def save_audio(full_pathname, filename):
+def save_audio(full_pathname, movie_title, filename):
     filename_stem = '.'.join(filename.split('.')[:-1])
 
     print "Converting %s to %s" % (full_pathname, os.path.join(mp3_dir, filename_stem + '.mp3'))
-    os.system('ffmpeg -i "%s" "%s"' % (full_pathname, os.path.join(mp3_dir, filename_stem + '.mp3')))
+    os.system('C:\\ffmpeg\\bin\\ffmpeg.exe -i "%s" "%s"' % (full_pathname, os.path.join(mp3_dir, movie_title + '.mp3')))
 
 def main():
     # Go through all directories
@@ -21,7 +21,8 @@ def main():
         # Only look at non-empty directories
         if len(filenames) > 0:
             for filename in filenames:
-                save_audio(os.path.join(dirname, filename), filename)
+                movie_title = dirname.split('\\')[-1]
+                save_audio(os.path.join(dirname, filename), movie_title, filename)
 
 if __name__ == '__main__':
     main()
